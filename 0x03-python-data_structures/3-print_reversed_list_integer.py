fbb731303872aa936be3155184309b541dfb00d9
XimeonLeo <ximeonleo@gmail.com>
2023-06-12 21:52:22 +0000

    added more edhe cases by using the index of the list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list is not None:
-        for elem in range(len(my_list), 0, -1):
-            print("{:d}".format(elem))
+        length = len(my_list)
+        for elem in range(length - 1, -1, -1):
+            print("{:d}".format(my_list[elem]))
