5021fccc552a1631287f23c7a9a44c2a93aa36d0
XimeonLeo <ximeonleo@gmail.com>
2023-06-12 21:44:10 +0000

    included more edge cases incase we have an empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for elem in range(len(my_list), 0, -1):
-        print("{}".format(elem))
+    if my_list is not None:
+        for elem in range(len(my_list), 0, -1):
+            print("{:d}".format(elem))
