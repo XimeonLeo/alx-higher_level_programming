8e5a8a153ea168ac905977b90d39dd5f255f58f8
XimeonLeo <ximeonleo@gmail.com>
2023-06-14 14:34:27 +0000

    fixed the extra space at the end of a row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for elem in row:
+    for row in range(len(matrix)):
+        for elem in range(len(matrix[row])):
             if elem != 0:
                 print(" ", end='')
-            print("{:d}".format(elem), end='')
+            print("{:d}".format(matrix[row][elem]), end='')
         print()
