6682384d8336f11d74605e310fa90b9e1cc94115
XimeonLeo <ximeonleo@gmail.com>
2023-07-08 13:49:05 +0000

    testing

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -16,10 +16,11 @@ def text_indentation(text):
     x = 0
     while x < len(text):
         print(f"{text[x]}", end='')
-        if text[x] in [".", "?", ":"]:
-            print("\n")
-            y = x + 1
-            while text[y] == " ":
+        if text[x] == '\n' or text[x] in [".", "?", ":"]:
+            if text[x] in [".", "?", ":"]:
+                print("\n")
+            x += 1
+            while x < len(text) and text[x] == " ":
                 x += 1
-                y += 1
+            continue
         x += 1
