ae452d8fa5fe719c2538007b9806d4fd7c332311
XimeonLeo <ximeonleo@gmail.com>
2023-07-11 21:41:26 +0000

    checks for instancws

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A module that checks if an object is 
+    exactly an instance of the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """ Returns True if the object and class are of the same instance 
+        Returns False if not
+
+        Arguments:
+                    obj: the object passed
+                    a_class: the class to chexk
+    """
+    if type(obj) == a_class:
+        return True
+    return False
