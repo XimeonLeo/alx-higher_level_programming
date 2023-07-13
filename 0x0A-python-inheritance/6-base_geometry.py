fb706b45d86ce22a9941c34250048ba4769459d5
XimeonLeo <ximeonleo@gmail.com>
2023-07-13 00:34:04 +0000

    added a method area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A module that creates an empty class """
+
+
+class BaseGeometry:
+    """ Defines a class BaseGeaometry """
+
+    def area(self):
+        """ Returns the area of what is to come """
+
+        raise Exception("area() is not implemented")
