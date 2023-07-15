042cd4e1ac6a20e3207f17b11e84da47f9a4b5b2
XimeonLeo <ximeonleo@gmail.com>
2023-07-15 18:32:04 +0000

    python to jason

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" A module that that retuens a JSON rep of an obj """
+import json                                                            
+
+def to_json_string(my_obj):                                                """My json comverter function
+                                                                           Args:                                                                  |   my_obj: a python string                                                                                                                   Return:                                                                |   ¦   tge json representation of python                              """                                                                    return json.dumps(my_obj)
