dfa3deb3efda07d4752400e15bfad796c0b530f7
XimeonLeo <ximeonleo@gmail.com>
2023-07-16 20:21:30 +0000

    add args to a list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A module that adds all args to a list """
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+try:
+    args_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    args_list = []
+
+for arg in sys.argv[1:]:
+    args_list.append(arg)
+save_to_json_file(args_list, "add_item.json")
