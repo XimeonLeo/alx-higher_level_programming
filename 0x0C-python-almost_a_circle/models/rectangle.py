17e382d59d01577ed8b074a371d6be6ddddf0867
XimeonLeo <ximeonleo@gmail.com>
2023-07-17 20:20:17 +0000

    updates

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,9 +18,13 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """ Sets the value for width """
-        self.__width = width
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -28,9 +32,13 @@ class Rectangle(Base):
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """ Sets the value for height """
-        self.__height = height
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
@@ -38,9 +46,13 @@ class Rectangle(Base):
         return self.__x
 
     @x.setter
-    def x(self, x):
+    def x(self, value):
         """ sets the value for x """
-        self.__x = x
+        if type(value) != int:
+            raise TypeError("x must be an intrger")
+        elif value < 0:
+            raise ValueError("x must be > 0")
+        self.__x = value
 
     @property
     def y(self):
@@ -48,6 +60,32 @@ class Rectangle(Base):
         return self.__y
 
     @y.setter
-    def y(self, y):
+    def y(self, value):
         """ Sets the value for y """
-        self.__y = y
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be > 0")
+        self.__y = value
+
+    def area(self):
+        """ Returns the area of the Rectangle """
+        return self.__height * self.__width
+
+    def display(self):
+        """ Prints the rectangle to stdout """
+        for cm in range(self.area()):
+            if cm % self.__width == 0:
+                if cm != 0:
+                    print()
+            print('#', end='')
+        print()
+
+    def __str__(self):
+        """ Returns [Rectangle] (<id>) <x>/<y> - <width>/<height> """
+        idd = self.id
+        xx = self.__x
+        yy = self.__y
+        wit = self.__width
+        hit = self.__height
+        return f"[Rectangle] ({idd}) {xx}/{yy} - {wit}/{hit}"
