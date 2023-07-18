f1c13859c2f3c5c6a464e030af54dde80e13813c
XimeonLeo <ximeonleo@gmail.com>
2023-07-18 22:06:05 +0000

    Update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,21 @@ class Base:
             else:
                 lst = [att.to_dictionary() for att in list_objs]
             my_file.write(Base.to_json_string(lst))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the python rep of json """
+        if not json_string:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Returns an inatance with all attr set """
+        if __name__ == 'Square':
+            dummy = cls(5, 6, 7)
+        else:
+            dummy = cls(2, 4, 8, 9)
+        dummy.update(**dictionary)
+        return dummy
