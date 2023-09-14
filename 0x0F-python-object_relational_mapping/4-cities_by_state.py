934f8ad08ed8f2f902e0d12e8fbeb71ea3dd0fed
XimeonLeo <ximeonleo@gmail.com>
2023-09-14 03:13:51 -0700

    pushing to other computer

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,4 +4,18 @@ import MySQLdb
 import sys
 
 
-if __name__ == '
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cusr = db.cursor()
+    cusr.execute("SELECT c.id, c.name, s.name\
+                    FROM cities AS c\
+                        INNER JOIN states AS s\
+                             ON c.state_id = s.id")
+
+    cities = cusr.fetchall()
+
+    for city in cities:
+        print(city)
+
+    cusr.close()
+    db.close()
