7ae1f56e1525f220887c98bd6b0812fc0886b56f
XimeonLeo <ximeonleo@gmail.com>
2023-09-15 19:39:02 -0700

    Adding a new state object to the Table State

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" A module that adds a state object to State table in database
+    hbtn_0e_6_usa7
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    _user = sys.argv[1]
+    _passwd = sys.argv[2]
+    _db = sys.argv[3]
+
+    # establishing connection to the database
+    engine = create_engine(
+        f"mysql+mysqldb://{_user}:{_passwd}@localhost:3306/{_db}",
+        pool_pre_ping=True
+            )
+    # creating all tables related to the Base metadata
+    Base.metadata.create_all(engine)
+
+    # create session instance of the engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the table
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    # printing the nee state id
+    print(new_state.id)
+
+    # Closing session
+    session.close()
