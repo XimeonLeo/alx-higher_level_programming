d829ca8b15897802123a977548a68d6e46b3c7d5
XimeonLeo <ximeonleo@gmail.com>
2023-09-30 04:20:11 +0100

    erroe code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A module that sends a requeat to a url and displays
+    tge body of the response in utf-8
+"""
+import urllib.request
+import sys
+
+
+if __name__ == '__main__':
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as reaponse:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
