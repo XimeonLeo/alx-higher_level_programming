1b15e079aa20599e05d42937ef521177e3cfc1a4
XimeonLeo <ximeonleo@gmail.com>
2023-10-01 11:09:54 -0700

    using utf-8 instrad of ascii

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     print(f"Body response:")
     print(f"\t- type: {type(html)}")
     print(f"\t- content : {html}")
-    print(f"\t- utf8 content: {html.decode('ascii')}")
+    print(f"\t- utf8 content: {html.decode('utf-8')}")
