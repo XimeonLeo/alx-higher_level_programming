3db8ed90452cf6b20afe0c381daed64a5de7704b
XimeonLeo <ximeonleo@gmail.com>
2023-10-01 11:04:27 -0700

    replaced spaces with tabs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ import requests
 url = 'https://alx-intranet.hbtn.io/status'
 _request = requests.get(url)
 print("Body response:")
-print(f"    - type: {type(_request.text)}")
-print(f"    - content: {_request.text}")
+print(f"\t- type: {type(_request.text)}")
+print(f"\t- content: {_request.text}")
