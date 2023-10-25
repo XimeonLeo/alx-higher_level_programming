ee31b7ede792c827709b2adcdb9f080b6acb2059
XimeonLeo <ximeonleo@gmail.com>
2023-10-25 03:31:11 +0100

    moving computer

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,13 +11,14 @@ request.get(url, (err, res, body) => {
   }
   const todoList = JSON.parse(body);
   const completed = todoList.filter((dict) => dict.completed);
-  completed.forEach(todo => {
-    if (todo.userId) {
-      todo.userId++;
+  const completedDict
+  completed.forEach(todo) => {
+    if (completedDict[todo.userId]) {
+      completedDict[todo.userId]++;
     } else {
-        todo.userId = 1;
+        completedDict[todo.userId] = 1;
     }
-  });
-  console.log(completed);
   }
-})
+  console.log(completed)Dict;
+  }
+});
