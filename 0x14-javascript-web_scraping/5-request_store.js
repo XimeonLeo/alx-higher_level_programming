cf3b46f4e407d02a5871bea5aa0dcc08c668e0bb
XimeonLeo <ximeonleo@gmail.com>
2023-10-24 18:41:41 -0700

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,16 +7,15 @@ const url = process.argv[2];
 const fs = require('fs');
 const filePath = process.argv[3];
 
-request.get(url, (err, res, body) =>{
+request.get(url, (err, res, body) => {
   if (err) {
     console.error(err);
     return;
   }
   // This block writes into the file if GET was succesful
-  fs.writeFile(filePath, body, { flag: "w" }, (error) => {
+  fs.writeFile(filePath, body, { flag: 'w' }, (error) => {
     if (error) {
       console.error(error);
-      return;
     }
   });
 });
