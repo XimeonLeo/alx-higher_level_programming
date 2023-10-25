e82ac37458c57a5d062d3e3f5eb835f31cc17ed8
XimeonLeo <ximeonleo@gmail.com>
2023-10-25 02:40:13 +0100

    moving computer

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const url = process.argv[2];
+
+const fs = require('fs');
+const filePath = process.argv[3];
+
+request.get(url, (err, res, body) =>{
+  if (err) {
+    console.error(err);
+    return;
+  }
+  // This block writes into the file if GET was succesful
+  fs.writeFile(filePath, body, { flag: "w" }, (error) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+  });
+});
